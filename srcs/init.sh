b32280fdf272751b9211a81bc794bd6327566fe9
averygan <averygan19@gmail.com>
2025-04-29 20:40:51 +0800

    original makefile

--- srcs/init.sh
+++ srcs/init.sh
@@ -1,4 +1,4 @@
 # create directories for volumes to be mounted
 mkdir -p ~/data/mariadb_data ~/data/wordpress_files
-chown -R $USER:staff ~/data/wordpress_files ~/data/mariadb_data
-chmod -R 777 ~/data/wordpress_files ~/data/mariadb_data
+sudo chown -R $USER:staff ~/data/wordpress_files ~/data/mariadb_data
+sudo chmod -R 777 ~/data/wordpress_files ~/data/mariadb_data
